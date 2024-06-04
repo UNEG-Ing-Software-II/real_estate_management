532213abf6d81e6446d534687e92f5394bd45c30
Henry Navarro <henrymanuelnavarro@gmail.com>
2024-06-03 22:21:32 -0400

    [UPDATE] settings.py

--- MyR/settings.py
+++ MyR/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "MyR",
         "USER": "postgres",
-        "PASSWORD": "diaz3214",
+        "PASSWORD": "password",
         "HOST": "localhost",
         "DATABASE_PORT": "5432",
     }
