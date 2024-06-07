41e00b9e0d8bbbd60eb5b897db1012dedc9987f0
Pablish2001 <91689057+Pablish2001@users.noreply.github.com>
2024-06-06 19:21:30 -0600

    [correpciones] arreglar proteccion de vistas
    hacer que el logo redireccione a la vista dependiendo del rol

--- app_Principal/Controladores/InmuebleController.py
+++ app_Principal/Controladores/InmuebleController.py
@@ -1,6 +1,8 @@
 # forms.py
 from django import forms
 from app_Principal.models import *
+from django.contrib.auth.decorators import login_required
+from MyR.backend import role_required     
         
 class InmuebleForm(forms.ModelForm):
     class Meta:
@@ -14,14 +16,18 @@ class InmuebleForm(forms.ModelForm):
 
 # views.py
 from django.shortcuts import render, redirect, get_object_or_404
-
+@login_required(login_url='login')
+@role_required('Asesor')
 def registrar_inmueble(request):
     if request.method == 'POST':
         form = InmuebleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('inicio Asesor')
+    return redirect('inicio_Asesor')
 
+@login_required(login_url='login')
+@role_required('Asesor')
 def modificar_inmueble(request, inmueble_id):
     # Obtener el inmueble por su ID
     inmueble = get_object_or_404(Inmueble, id=inmueble_id)
@@ -30,13 +36,17 @@ def modificar_inmueble(request, inmueble_id):
         if form.is_valid():
             form.save()
             return redirect('inicio Asesor')
+    return redirect('inicio_Asesor')
 
+@login_required(login_url='login')
+@role_required('Asesor')
 def eliminar_inmueble(request, inmueble_id):
     # Obtener el inmueble por su ID
     inmueble = get_object_or_404(Inmueble, id=inmueble_id)
     if request.method == 'POST':
         inmueble.delete()
         return redirect('inicio Asesor')
+    return redirect('inicio_Asesor')
 
 def inmuebles_registrados(request):
     inmuebles = Inmueble.objects.all()
