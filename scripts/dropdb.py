e46829cfb400057efdf6b7053e42fe8f61d2be04
Henry Navarro <henrymanuelnavarro@gmail.com>
2024-06-22 17:13:27 -0400

    [UPDATE] dropdb.py

--- scripts/dropdb.py
+++ scripts/dropdb.py
@@ -1,5 +1,12 @@
 # This erases all the Railway and local tables, use when the BD is updated
+import os
+import sys
 from django.db import connection
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyR.settings")
 
 def drop_tables():
     with connection.cursor() as cursor:
